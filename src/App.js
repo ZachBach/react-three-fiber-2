736c2acdf9f595694e61b78b633f6e105aac33bb
Zach Auerbach <auernachzach@gmail.com>
2021-01-24 21:54:00 -0800

    added more comments on how to use box/circle buffer geo as well as the useFrame() hook

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.scss';
 
 // import canvas from fiber
-import { Canvas } from 'react-three-fiber';
+import { Canvas, useFrame } from 'react-three-fiber';
 
 function App() {
   return (
     <>
       <Canvas>
-        <mesh></mesh>
+        <mesh>
+          <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
+          <meshStandardMaterial attach='material' />
+        </mesh>
       </Canvas>
     </>
   );
@@ -21,6 +24,17 @@ export default App;
 // first define a mesh(inside the mesh define a material and geometry)
 // Material: looks/color and Geometry: Shape
 
+// next we define our boxBufferGeometry this can be self closing if desired.
+// this also has a required attribute attach='geomtery' followed by the args.
+// the arguments are three things the general size of the object,
+// represent the height, width, &  depth inside of an array.
+//                 x       y         z
+
+// <circleBufferGeometry attach='geometry' args={[2, 100]} />
+// these two args are for the [size, ammount of angles]
+
+// Next we provide just a standard mesh material
+
 // Mesh: Class representing triangular polygen mesh based objects
 
 // BufferGeometry: An efficient representation of mesh,line,or point geometry.
@@ -30,3 +44,11 @@ export default App;
 // Material: Materials describe the appearance of objects
 // they are defined in a (mostly) renderer independent way so you don't have to
 // rewrite materials if you decide to use a different renderer.
+
+// useFrame(): This hook calls you back every frame, which is good for running effects
+// updating controls, etc. You receive the state( same as useThree) and a clock delta.
+// If you supply a render priority greater than zero it will switch off automatic rendering
+// entirely, you can then control rendering yourself. If you have multiple frames with a
+// render priority then they are ordered highest priority last, similar to z-index.
+// Frames are managed, three fiber will remove them automatically when the component that
+// holds them is unmounted.
